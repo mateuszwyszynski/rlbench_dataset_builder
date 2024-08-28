9e00587180f5375112ca0319925aacbb5cc99fa9
Mateusz <mat.wyszynski@gmail.com>
2024-08-28 10:14:48 +0200

    feat: adjusted builder to work with RLBench

--- rlbench_dataset/rlbench_dataset_builder.py
+++ rlbench_dataset/rlbench_dataset_builder.py
@@ -5,18 +5,23 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_hub as hub
+import pickle
+from PIL import Image
 
 
-class ExampleDataset(tfds.core.GeneratorBasedBuilder):
+class RLBenchDataset(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for example dataset."""
 
-    VERSION = tfds.core.Version('1.0.0')
+    VERSION = tfds.core.Version('4.0.0')
     RELEASE_NOTES = {
       '1.0.0': 'Initial release.',
+      '3.0.0': 'Observation with joint positions.',
+      '4.0.0': 'Pick and lift task'
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.rlbench_generated_dataset_path = "../../generated_datasets/pick_and_lift"
         self._embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
     def _info(self) -> tfds.core.DatasetInfo:
@@ -26,29 +31,27 @@ class ExampleDataset(tfds.core.GeneratorBasedBuilder):
                 'steps': tfds.features.Dataset({
                     'observation': tfds.features.FeaturesDict({
                         'image': tfds.features.Image(
-                            shape=(64, 64, 3),
+                            shape=(256, 256, 3),
                             dtype=np.uint8,
                             encoding_format='png',
                             doc='Main camera RGB observation.',
                         ),
                         'wrist_image': tfds.features.Image(
-                            shape=(64, 64, 3),
+                            shape=(256, 256, 3),
                             dtype=np.uint8,
                             encoding_format='png',
                             doc='Wrist camera RGB observation.',
                         ),
-                        'state': tfds.features.Tensor(
-                            shape=(10,),
+                        'joint_positions': tfds.features.Tensor(
+                            shape=(6,),
                             dtype=np.float32,
-                            doc='Robot state, consists of [7x robot joint angles, '
-                                '2x gripper position, 1x door opening angle].',
+                            doc='6x robot joint angles.',
                         )
                     }),
                     'action': tfds.features.Tensor(
-                        shape=(10,),
+                        shape=(7,),
                         dtype=np.float32,
-                        doc='Robot action, consists of [7x joint velocities, '
-                            '2x gripper velocities, 1x terminate episode].',
+                        doc='Robot action for joints in one arm + gripper',
                     ),
                     'discount': tfds.features.Scalar(
                         dtype=np.float32,
@@ -90,36 +93,48 @@ class ExampleDataset(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Define data splits."""
         return {
-            'train': self._generate_examples(path='data/train/episode_*.npy'),
-            'val': self._generate_examples(path='data/val/episode_*.npy'),
+            'train': self._generate_examples(path=self.rlbench_generated_dataset_path),
+            # 'val': self._generate_examples(path='data/val/episode_*.npy'),
         }
 
     def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
         """Generator of examples for each split."""
 
-        def _parse_example(episode_path):
-            # load raw data --> this should change for your dataset
-            data = np.load(episode_path, allow_pickle=True)     # this is a list of dicts in our case
+        def _parse_episode(episode_path, variation_description):
+
+            low_dim_obs_path = episode_path + "/low_dim_obs.pkl"
+
+            with open(low_dim_obs_path, 'rb') as file:
+                low_dim_obs = pickle.load(file)
 
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
             episode = []
-            for i, step in enumerate(data):
+
+            for i, step in enumerate(low_dim_obs):
                 # compute Kona language embedding
-                language_embedding = self._embed([step['language_instruction']])[0].numpy()
+                language_embedding = self._embed([variation_description])[0].numpy()
+
+                front_image_path = episode_path + f"/front_rgb/{i}.png"
+                wrist_image_path = episode_path + f"/wrist_rgb/{i}.png"
+
+                front_image = Image.open(front_image_path)
+                wrist_image = Image.open(wrist_image_path)
+                action = np.concatenate([step["joint_velocities"], [step["gripper_open"]]], axis=-1, dtype=np.float32)
+                joint_positions = np.array(step["joint_positions"], dtype=np.float32)
 
                 episode.append({
                     'observation': {
-                        'image': step['image'],
-                        'wrist_image': step['wrist_image'],
-                        'state': step['state'],
+                        'image': np.array(front_image),
+                        'wrist_image': np.array(wrist_image),
+                        'joint_positions': joint_positions,
                     },
-                    'action': step['action'],
+                    'action': action,
                     'discount': 1.0,
-                    'reward': float(i == (len(data) - 1)),
+                    'reward': float(i == (len(low_dim_obs) - 1)),
                     'is_first': i == 0,
-                    'is_last': i == (len(data) - 1),
-                    'is_terminal': i == (len(data) - 1),
-                    'language_instruction': step['language_instruction'],
+                    'is_last': i == (len(low_dim_obs) - 1),
+                    'is_terminal': i == (len(low_dim_obs) - 1),
+                    'language_instruction': variation_description,
                     'language_embedding': language_embedding,
                 })
 
@@ -132,19 +147,37 @@ class ExampleDataset(tfds.core.GeneratorBasedBuilder):
             }
 
             # if you want to skip an example for whatever reason, simply return None
-            return episode_path, sample
+            return episode_path + variation_description, sample
 
-        # create list of all examples
-        episode_paths = glob.glob(path)
+        variations_paths = glob.glob(path + "/variation*")
 
-        # for smallish datasets, use single-thread parsing
-        for sample in episode_paths:
-            yield _parse_example(sample)
+        for variation in variations_paths:
+            variation_descriptions_path = variation + "/variation_descriptions.pkl"
+            with open(variation_descriptions_path, 'rb') as file:
+                variation_descriptions = pickle.load(file)
+
+            variation_description_index = 0
+
+            episodes_paths = glob.glob(variation + "/episodes/episode*")
+            for episode_path in episodes_paths:
+                yield _parse_episode(episode_path, variation_descriptions[variation_description_index])
+                variation_description_index = (variation_description_index + 1) % len(variation_descriptions)
+
+
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
+        # paths = []
+        # variations_paths = glob.glob(path + "/variation*")
+        # for variation in variations_paths:
+        #     variation_descriptions_path = variation + "/variation_descriptions.pkl"
+        #     with open(variation_descriptions_path, 'rb') as file:
+        #         variation_descriptions = pickle.load(file)
+        #     for variation_description in variation_descriptions:
+        #         episodes_paths = glob.glob(variation + "/episodes/episode*")
+        #         for episode_path in episodes_paths:
+        #             paths.append((episode_path, variation_description))
         # beam = tfds.core.lazy_imports.apache_beam
         # return (
-        #         beam.Create(episode_paths)
-        #         | beam.Map(_parse_example)
+        #         beam.Create(paths)
+        #         | beam.Map(_parse_episode)
         # )
-
